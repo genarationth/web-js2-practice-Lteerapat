896e754eb5f8dffecc03a92b4edb948efe08f0e1
Teerapat Limpawittayakul <tlimpawittayakul@gmail.com>
2023-03-10 20:48:08 +0700

    create information_card.js

---
+++ information_card.js
@@ -0,0 +1,5 @@
+const your_name = "John";
+const location = "South Africa";
+const hobby = "drinking homemade wine";
+
+console.log(`Hi, my name is ${your_name}. I live in ${location} and enjoy ${hobby}.`)
